88fd33f295a19d5674d97b88fa50319282c069ca
Alex Shisler <atshisler@gmail.com>
2018-10-18 23:14:45 -0400

    Initial commit for Project 0

---
+++ Alex_Shisler_Code/BankingProject/src/main/java/com/revature/driver/Driver.java
@@ -0,0 +1,14 @@
+package com.revature.driver;
+
+import com.revature.services.BankServices;
+import com.revature.view.BankViewer;
+
+public class Driver {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		BankViewer niceView = new BankViewer();
+		niceView.accountOptions();
+	}
+
+}
